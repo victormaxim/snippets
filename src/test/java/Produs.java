55075c60f7528daf8c298d317bb3fe6ae475d3e5
adima <adima@deloittece.com>
2018-06-27 11:28:56 +0300

    snippets: ba da boom

---
+++ src/test/java/Produs.java
@@ -0,0 +1,9 @@
+public class Produs {
+
+    String nume;
+
+    public static void main(String[] args) {
+
+    }
+
+}
--- src/test/java/Snippets.java
+++ src/test/java/Snippets.java
@@ -1,15 +1,31 @@
 import org.junit.Test;
+//import org.openqa.selenium.interactions.Actions;
 
 public class Snippets {
+    private By popUpOk = By.xpath("//table//button[text()='OK']");
 
     @Test
-    public void SnippetA(){
+    public void clickWithActions(){
+        WebElement isPopUp = waitForElement(this.popUpOk);
+                WebElement secondPopUp = waitForElement(this.popUpOk);
+                Actions actions = new Actions(driver);
+                if (isPopUp  != null && isPopUp.isDisplayed()) {
+                        actions.moveToElement(isPopUp).click().perform();
+                    }
+                Thread.sleep(500);
+                if (secondPopUp  != null && isPopUp.isDisplayed()) {
+                        actions.moveToElement(secondPopUp).click().perform();
 
     }
 
     public static void main(String[] args) {
 
+        try{
+
+        }
+
     }
+
 
 
 
